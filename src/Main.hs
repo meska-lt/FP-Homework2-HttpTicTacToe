afe7d30567b283f188bdb4c22bd98d33e1f2bbc2
Robertas <meska.lt@gmail.com>
2015-12-21 23:34:25 +0200

    Already able to automatically play more than one round.

--- src/Main.hs
+++ src/Main.hs
@@ -7,6 +7,9 @@ import Network.URI
 
 import Parser
 
+maxRoundAmount :: Int
+maxRoundAmount = 10
+
 programStartMessage :: String
 programStartMessage = "Programa darbą pradėjo."
 
@@ -57,31 +60,37 @@ executeNetworkingWithId :: String -> IO ()
 executeNetworkingWithId gameId = do
   putStrLn ("Zaidimo id: " ++ gameId)
   connection <- openStream "tictactoe.homedir.eu" 80
-  let serial = serializeBoard $ concat $ randomMove emptyBoard X 0 :: String
-  makeMoveViaConnection connection gameId serial 0 0
-  makeMoveViaConnection connection gameId serial 0 1
+  makeRoundViaConnection connection gameId 0
 
 makeMoveViaConnection :: HandleStream String -> String -> String -> Int -> Int -> IO ()
 makeMoveViaConnection connection gameId gameState round move =
     if (mod move 2) == 1
         then do
                 let serial = ""
-                rawResponse <- sendHTTP connection (getRequestWithIdAndState gameId serial)
+                rawResponse <- sendHTTP connection (getRequestWithIdAndState (gameId ++ (show round)) serial)
                 body <- getResponseBody rawResponse
                 putStrLn ("GET Received: " ++ body)
                 if ((move < 9) && (getWinner body == Nothing))
                     then makeMoveViaConnection connection gameId body round (move + 1)
-                    else return()
+                    else makeRoundViaConnection connection gameId (round + 1)
         else if (mod move 2) == 0
             then do
                     putStrLn ("POST Sent: " ++ gameState)
-                    rawResponse <- sendHTTP connection (postRequestWithIdAndState gameId gameState)
+                    rawResponse <- sendHTTP connection (postRequestWithIdAndState (gameId ++ (show round)) gameState)
                     body <- getResponseBody rawResponse
                     putStrLn ("POST Received: " ++ body)
                     if ((move < 9) && (getWinner gameState == Nothing))
                         then makeMoveViaConnection connection gameId gameState round (move + 1)
-                        else return()
+                        else makeRoundViaConnection connection gameId (round + 1)
             else return()
+
+makeRoundViaConnection :: HandleStream String -> String  -> Int -> IO ()
+makeRoundViaConnection connection gameId round =
+    if round < maxRoundAmount
+        then do
+                let serial = serializeBoard $ concat $ randomMove emptyBoard X 0 :: String
+                makeMoveViaConnection connection gameId serial round 0
+        else printFinishMessage True
 
 main :: IO ()
 main = do
